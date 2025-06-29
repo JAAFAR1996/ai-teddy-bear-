ca6f4ccd34ba5eb1cdde199e25593df3f0fe26e0
JAAFAR1996 <138117801+JAAFAR1996@users.noreply.github.com>
2025-06-29 20:23:27 +0300

    Auto-backup Sun 06/29/2025 20:23:27.30

--- github_auto_backup.bat
+++ github_auto_backup.bat
@@ -5,8 +5,7 @@ git add -A
 
 git commit -m "Auto-backup %DATE% %TIME%"
 
-git push https://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@github.com/JAAFAR1996/ai-teddy-bear-.git main
-
+git push https://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@github.com/JAAFAR1996/ai-teddy-bear-.git main
 git status
 git log -1
 pause
